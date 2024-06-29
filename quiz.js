d995dbb154f5990ec7f695dba3dccfd782633a11
Brian Hang <hangwithbrian@gmail.com>
2024-06-29 00:52:02 -0700

    Fix wording

--- quiz.js
+++ quiz.js
@@ -1,6 +1,6 @@
 const questions = [
   {
-    question: "You're magically teleported to a town until night time. There is a gated entrance and next to it is a sign that says \"Dim Sum Dale\". As you walk past the gates, what's your first move?",
+    question: "You're magically teleported to a town. Don't worry though, you'll be brought back at night. Anyways, there is a gated entrance, and next to it is a sign that says \"Dim Sum Dale\". As you walk past the gates, what's your first move?",
     options: [
       ['Excitedly introduce yourself to the first dim sum person you meet', 'E'],
       ['Find a quiet bamboo basket to sit in and think about if you are a dim sum dish too', 'I'],
@@ -13,23 +13,24 @@ const questions = [
       ['Glance at the map quickly and pick a random area to explore', 'P'],
       ['Make a note of where everything is and plan out your day', 'J'],
     ],
+    image: 'info_booth.png',
   },
   {
-    question: 'To the right of the information booth is a small shop. A siu mai leaves the shop and notices you, and starts approaching.',
+    question: 'To the right of the information booth is a small shop. A siu mai leaves the shop and notices you! It starts approaching you.',
     options: [
       ["I don't see any legs, how is it moving?", 'S'],
       ['I wonder what it is thinking, will it think I am normal? What is normal for dim sum?', 'N'],
     ],
   },
   {
-    question: 'The siu mai introduces itself and notices you are a new face. It mentions you arrived just in time for the annual town parade! It mentions the parade is in an 30 minutes',
+    question: 'The siu mai introduces itself since you are a new face in the town. It mentions you arrived just in time for the annual parade!',
     options: [
       ['Ask it when and where the parade is so you can get there early and find a good view', 'J'],
       ['Explore a bit more before figuring out where the parade is later', 'P'],
     ],
   },
   {
-    question: 'It says it just got groceries and was about to cook with friends after the parade. It invites you to join them later!',
+    question: 'The siu mai just got groceries and is going to cook with its friends after the parade. You are invited to join them later.',
     options: [
       ['Accept the invitation, this is a chance to get to know who else is living here', 'E'],
       ["Thank it for the invitation, but say you don't want to bother them. What if you can't even eat what they eat?", 'I'],
@@ -39,21 +40,21 @@ const questions = [
     question: 'The parade starts, and floats start moving down the main road.',
     options: [
       ['Admire all the pretty designs, constumes, and music being played', 'S'],
-      ['Think about what dim sum culture and history is like', 'N'],
+      ['Think about what dim sum culture and history as the floats pass by', 'N'],
     ],
   },
   {
-    question: 'The parade is now ending. The mayor of the town shows up and announces there is a "best float" contest as well. Everyone is given a ballot to vote for the best float.',
+    question: 'The parade is now ending. The mayor of the town shows up and announces there is a "best float" contest. Everyone is given a ballot to vote for the best float. You pick the...',
     options: [
-      ['Choose the one that required the most craftsmanship', 'T'],
-      ['Choose the one that expressed the most personality', 'F'],
+      ['one that required the most craftsmanship', 'T'],
+      ['one that expressed the most personality', 'F'],
     ],
   },
   {
-    question: "It's now night time. Suddenly giant chopsticks from the sky pick you up and return you to everyday human life. As you are being lifted away, a village elder yells some life advice.",
+    question: "It's now night time. Suddenly giant chopsticks from the sky pick you up to return you to everyday human life! As you are being lifted away, the siu mai from before yells some life advice.",
     options: [
-      ["Thank the elder for advice and analyze the elder's words. Does dim sum advice apply to a human?", 'T'],
-      ["Appreciate the kindness of the elder. I'm glad a dim sum cares!", 'F'],
+      ["Thank the siu mai for advice and analyze the its words. Does dim sum advice even apply to a human?", 'T'],
+      ["Appreciate the kindness of the siu mai. I'm glad a dim sum cares!", 'F'],
     ],
   },
 ];
@@ -97,18 +98,20 @@ function loadQuestion() {
       ${imageHtml}
       <div class="options">
         ${question.options.map(([label, key]) => `
-            <button onclick="selectOption('${key}')">${label}</button>
+            <button onclick="selectOption(this, '${key}')">${label}</button>
         `).join('')}
       </div>
     </div>
   `;
 }
 
-function selectOption(option) {
+function selectOption(event, option) {
   scores[option]++;
   currentQuestionIndex++;
 
   loadQuestion();
+
+  event.target.blur();
 }
 
 function shouldShowBonusQuestion() {
